2ebd36dfa15b238864b6f90ecdab24864709f5b7
Jarrod Dowalter <35478547+jarroddowalter@users.noreply.github.com>
2023-01-25 12:37:38 -0800

    save

--- src/pygow-poker/script.py
+++ src/pygow-poker/script.py
@@ -1,9 +1,18 @@
 import random
+from typing import Union
 
 class Card:
 	def __init__(self, value: int, suit: str):
-		self.value = value
-		self.suit = suit
+		self._value = value
+		self._suit = suit
+
+	@property
+	def value(self):
+		return self._value
+
+	@property
+	def suit(self):
+		return self._suit
 
 	def pretty(self): ##returns pretty string of card
 		pretty_card = ""
@@ -53,8 +62,12 @@ class Deck:
 	SUIT_ORDER = ("d", "h", "c", "s")
 
 	def __init__(self):
-		self.cards = []
+		self._cards = []
 		self.build()
+
+	@property
+	def cards(self):
+		return self._cards
 
 	def build(self):
 		for v in range(2,14):
@@ -75,20 +88,92 @@ class Deck:
 		return pretty_deck
 
 class Hand:
-	def __init__(self, cards: list):
-		self.cards = cards
+	def __init__(self, cards: Union[object, list]):
+		self.validate_cards(cards)
+		self._cards = cards
+
+	@property
+	def cards(self):
+		return self._cards
 	
 	def sort(self):
 		pass
+		# result = list(hand)
+		# if len(result) == 1:
+		# 	return result
+		# else:
+		# 	result.sort()
+
+		# def get_card_suit_order(card: str):
+		# 	if card == "JKR":
+		# 		return 4  ## returns index of suit order
+		# 	for i, suit in enumerate(Deck.SUIT_ORDER):
+		# 		if card[-1] == suit:
+		# 			return i
+
+		# i = 1
+		# while i < len(result):
+		# 	if result[i][:2] == result[i - 1][:2] and get_card_suit_order(
+		# 		result[i]
+		# 	) < get_card_suit_order(result[i - 1]):
+		# 		result.insert(i - 1, result.pop(i))
+		# 		i = 1
+		# 	else:
+		# 		i += 1
+
+		 
+
+		# i = 0
+		# while i < len(self.cards):
+		# 	if cards[i]
+
+	def add(self, to_add: Union[object, list]):
+		self.validate_cards(to_add)
+
+		if isinstance(to_add, Card):
+			self.cards.append(to_add)
+		else:
+			self.cards.extend(to_add)
+			
+
+	def subtract(self, to_subtract: Union[object, list]):
+		result = self.validate_cards(to_subtract)
+
+		# i = 0
+		# while i < len(self.cards):
+		# 	if self.cards[i] in ts:
+		# 		self.cards.pop(i)
+		# 		i = 0
+		# 	else:
+		# 		i += 1
 
 	def get_outcome(self):
 		pass
 
+	def pretty(self):
+		pretty_hand = []
+		for card in self.cards:
+			pretty_hand.append(card.pretty())
+		return pretty_hand
+
+	def validate_cards(self, cards: Union[object, list]):
+		result = []
+		if type(cards) == object and not isinstance(cards, Card):
+			raise TypeError("Error: Object not instance of Card.")
+		elif type(cards) == list:
+			for card in cards:
+				if not isinstance(card, Card):
+					raise TypeError("Error: Items in list are not instances of Card.")
+
+		if type(cards) == object:
+			result = [cards]
+
+		return result
 class Bet:
 	def __init__(self, ante: int):
 		self.ante = ante
 
-	def add_all(self.__dict__):
+	def add_all(self):
 		pass
 
 class Player:
@@ -100,7 +185,11 @@ class Dealer:
 class Game:
 	pass
 
-bet = Bet(20)
-bet.a_high = 1
+c1 = Card(2, 's')
+c2 = Card(3, 'd')
+c3 = Card(4, 'h')
+c4 = Card(5, 'c')
 
-print(bet.__dict__)
+h1 = Hand(c3)
+h1.subtract(c3)
+print(h1.pretty())
